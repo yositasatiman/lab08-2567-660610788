7564ebebbced017a4779e3dfa1f9169b9914f7c3
yositasatiman <yosita_satiman@cmu.ac.th>
2024-07-20 12:21:46 +0700

    123

---
+++ src/app/components/Task.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export default function Task(props) {
+  const status = (isDone) => {
+    if(isDone === true) {
+      return "line-through";
+    } else {
+      return "";
+    }
+  }
+
+  return (
+    <div>
+      <div className="d-flex p-3 gap-2 align-items-center border-bottom">
+          <span style={{textDecoration: status(props.isDone)}}>{props.title}</span>
+          <button className="btn btn-success">Done</button>
+          <button className="btn btn-danger">Delete</button>
+        </div>
+    </div>
+  )
+}
---
+++ src/app/components/TaskInput.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function TaskInput() {
+  return (
+    <div className="d-flex gap-1">
+      <input
+        className="form-control"
+        placeholder="Insert a task here.."
+      ></input>
+      <button className="btn btn-primary">Add</button>
+    </div>
+  )
+}
