f033adb862c70c436463ce26041252c95afb8b6b
yositasatiman <yosita_satiman@cmu.ac.th>
2024-07-20 13:08:11 +0700

    112

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -4,8 +4,9 @@ export default function Footer({ year, fullName, studentId }) {
     return (
         <div className='mt-3'>
             <p className="text-secondary text-center">
-                &copy; {year} {fullName} ({studentId})
+                Copyright © {year} {fullName} ({studentId})
             </p>
         </div>
     );
 }
+
